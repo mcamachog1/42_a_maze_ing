e07b39e4e0f18852ce5ca19ffe100a454eb53f2d
Pedro Lopes <pelopes@c2r1s10.42porto.com>
2026-03-06 16:13:31 +0000

    amazing update read_config. Proxima etapa convert_values

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-
+from typing import List, Dict, Any, Tuple,  Optional, Union
 import sys
 
 
@@ -8,20 +8,61 @@ def main() -> None:
     if len(sys.argv) != 2:
         print("Usage:  python3 a_maze_ing.py <config.txt>")
         sys.exit()
+    config: Dict[str, Any] = read_config(sys.argv[1])
+    config = convert_values(config)
+    print(config)
+
+def read_config(filename: str) -> Dict[str, Any]:
+    config: Dict[str, Any] = {}
     try:
-        with open(sys.argv[1]) as f:
-            print(f"Leyendo archivo: {sys.argv[1]}")
+        mandatory_keys: List[str] = [
+            "WIDTH", 
+            "HEIGHT",
+            "ENTRY",
+            "EXIT",
+            "OUTPUT_FILE",
+            "PERFECT"
+            ]
+        all_keys: List[str] = mandatory_keys
+        all_keys.extend(["SEED", "ALGORITHM", "DISPLAY_MODE"])
+
+        with open(filename) as file:
+            for line in file:
+                line = line.strip()
+                if not line or line.startswith("#"):
+                    continue
+                key, value = line.split("=")
+                if key not in all_keys:
+                    raise ValueError
+                print(f"key={key}, value={value}")
+                config[key] = value
+            for key in mandatory_keys:
+                if key not in config.keys():
+                    raise ValueError
+
     except FileNotFoundError:
-        print("RESPONSE: Archive not found in storage matrix")
+        print("RESPONSE: Archive not found")
         sys.exit()
     except PermissionError:
-        print("RESPONSE:  Security protocols deny access")
+        print("RESPONSE: Archive deny access")
+        sys.exit()
+    except ValueError:
+        print('RESPONSE: Incorrect config format. Check "config.txt" file')
         sys.exit()
     except Exception as e:
         print(
             f"RESPONSE:  Unexpected error: {e}"
             f"- Type: {type(e).__name__}")
         sys.exit()
+    return config
+
+def convert_values(config: Dict[str, Any]) -> Dict[str, Any]:
+    #transformar width em int
+    #transformar height em int
+    #ENTRY e EXIT
+    #'PERFECT'
+    #SEED
+    pass
 
 
 if __name__ == "__main__":
